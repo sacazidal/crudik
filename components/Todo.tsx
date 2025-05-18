c79243e8d9293827ec8ad62e313f84b74cadcc8f
sacazidal <tw1nkle001@yandex.ru>
2025-05-19 00:07:39 +0700

    создал форму, инпут для ввода текста и кнопку создания(к ней POST запрос на создание задачи)

---
+++ components/Todo.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { TodoProps } from "@/types";
+import { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const Todo = ({ onAdd }: TodoProps) => {
+  const [title, setTitle] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+
+    await fetch("http://localhost:6743/api/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title }),
+    });
+
+    setTitle("");
+    onAdd();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mb-4">
+      <Input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Новая задача"
+        className="w-full p-2 border rounded-sm md:rounded-lg md:placeholder:text-base placeholder:text-sm md:h-9 h-7"
+      />
+      <Button
+        type="submit"
+        className="mt-2 md:h-full h-7 w-full bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 md:text-base text-sm"
+      >
+        Добавить
+      </Button>
+    </form>
+  );
+};
+export default Todo;
